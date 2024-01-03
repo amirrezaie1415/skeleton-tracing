e246c8ca4fc6c4271be652d2605ad1ac7b83bf32
Amir Rezaie <amir.rezaie@epfl.ch>
2024-01-03 16:03:51 +0100

    modify exapmle.py

--- swig/example.py
+++ swig/example.py
@@ -8,13 +8,6 @@ im = cv2.imread("../test_images/SVS_2_LS43_to_LS44_RS6_0000_0_mask.png", 0)
 # Apply threshold
 _, im = cv2.threshold(im, 128, 255, cv2.THRESH_BINARY)
 
-# Resize the image for display if it's too large
-max_display_size = 800  # Maximum display size
-h, w = im.shape[:2]
-if h > max_display_size or w > max_display_size:
-    scaling_factor = max_display_size / float(h) if h > w else max_display_size / float(w)
-    im = cv2.resize(im, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
-
 # Trace skeleton
 polys = trace_skeleton.from_numpy(im)
 
@@ -24,11 +17,19 @@ for l in polys:
     for i in range(0, len(l) - 1):
         cv2.line(im, (l[i][0], l[i][1]), (l[i + 1][0], l[i + 1][1]), c)
 
+# Create a resizable window with a valid name
+window_name = 'Display Window'
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+# Set the window size (change the width and height as needed)
+window_width, window_height = 800, 600
+cv2.resizeWindow(window_name, window_width, window_height)
+
 # Display the image
-cv2.imshow('', im)
+cv2.imshow(window_name, im)
 while True:
     # Break the loop when 'q' is pressed or the window is closed
-    if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty('', cv2.WND_PROP_VISIBLE) < 1:
+    if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
         break
 
 cv2.destroyAllWindows()
